1ed9776c514c3ad4c28f752cb25265a18edb7ef2
KaisarOrange <alif.ayodya@gmail.com>
2023-11-14 03:55:17 +0700

    commit changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import fs from 'fs';
 
 export default defineConfig({
 	plugins: [sveltekit(), purgeCss()],
@@ -9,12 +8,7 @@ export default defineConfig({
 		// proxy: {
 		// 	'/api': {
 		// 		target: 'http://127.0.0.1:8080',
-
 		// 	}
 		// }
-		https: {
-			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
-			cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
-		}
 	}
 });
