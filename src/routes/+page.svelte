c52c05b9d1e114707bbecffddfdff5fdfc72b2d4
KaisarOrange <alif.ayodya@gmail.com>
2023-11-15 02:05:42 +0700

    commit changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
 	console.log('gello: ', data);
 </script>
 
-<div class=" flex justify-between m-6"><Posts data={data.posts.data} /></div>
+<div class=" flex justify-between m-6"><Posts data={data.posts.data.ruang} /></div>
 
 <style>
 </style>
